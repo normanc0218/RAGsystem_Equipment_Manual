da11745b916b29ae49a4a60b28b99b66d204349f
normanc0218 <149454253+normanc0218@users.noreply.github.com>
2026-05-07 22:05:11 +0000

    rename subagent files

--- email_agent/sub_agents/__init__.py
+++ email_agent/sub_agents/__init__.py
@@ -1,8 +1,8 @@
-from .mailbox_sync import mailbox_sync_agent
-from .inbox_processing import inbox_processing_agent
-from .summarization import summarization_agent
-from .digest import digest_agent
-from .audit import audit_agent
+from .mailbox_sync_agent import mailbox_sync_agent
+from .inbox_processing_agent import inbox_processing_agent
+from .summarization_agent import summarization_agent
+from .digest_agent import digest_agent
+from .audit_agent import audit_agent
 
 __all__ = [
     "mailbox_sync_agent",
